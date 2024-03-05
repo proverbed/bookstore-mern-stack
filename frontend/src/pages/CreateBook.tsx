ddc0a76531af6cfc056e1a7002711107389a396a
Dmitri De Klerk <dmitriwarren@gmail.com>
2024-03-05 14:21:26 +0200

    Add formik validation on forms

--- frontend/src/pages/CreateBook.tsx
+++ frontend/src/pages/CreateBook.tsx
@@ -1,30 +1,40 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { BACKEND_URL } from "./Home";
 import axios from "axios";
 import { Button, Input } from "@material-tailwind/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 const CreateBook: React.FC = () => {
   const PATH: string = BACKEND_URL + "/books";
-  const [title, setTitle] = useState<string>("");
-  const [author, setAuthor] = useState<string>("");
-  const [publishYear, setPublishYear] = useState<number>(0);
   const navigate = useNavigate();
-  const handleSaveBook = () => {
-    const data = {
-      title,
-      author,
-      publishYear,
-    };
-    axios
-      .post(PATH, data)
-      .then(() => {
-        navigate("/");
-      })
-      .catch((error) => {
-        alert("An error occurred");
-      });
-  };
+  const bookSchema = Yup.object().shape({
+    title: Yup.string().required("Title is required"),
+    author: Yup.string().required("Author is required"),
+    publishYear: Yup.number()
+      .min(1000, `Publish year can't be less than 1000`)
+      .max(new Date().getFullYear(), `Publish year can't be in the future`)
+      .required("Publish year is required"),
+  });
+  const formik = useFormik({
+    initialValues: {
+      title: "",
+      author: "",
+      publishYear: "",
+    },
+    onSubmit: (values) => {
+      axios
+        .post(PATH, values)
+        .then(() => {
+          navigate("/");
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("An error occurred");
+        });
+    },
+    validationSchema: bookSchema,
+  });
   return (
     <div className="mx-auto w-full max-w-7xl">
       <section
@@ -36,53 +46,65 @@ const CreateBook: React.FC = () => {
         </div>
       </section>
       <section className="p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 leading-6">
-        <div className="flex  md:w-1/2 w-full flex-col gap-6">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex  md:w-1/2 w-full flex-col gap-6"
+        >
           <Input
             variant="outlined"
             label="title"
             placeholder="title"
-            required
-            value={title}
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
+            name="title"
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             crossOrigin={undefined}
           />
+          <div className="text-sm text-slate-700">
+            {formik.errors.title && formik.touched.title && formik.errors.title}
+          </div>
           <Input
             variant="outlined"
             label="author"
-            value={author}
+            value={formik.values.author}
             placeholder="author"
-            required
-            onChange={(e) => {
-              setAuthor(e.target.value);
-            }}
+            name="author"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             crossOrigin={undefined}
           />
+          <div className="text-sm text-slate-700">
+            {formik.errors.author &&
+              formik.touched.author &&
+              formik.errors.author}
+          </div>
           <Input
             variant="outlined"
             label="publish year"
+            name="publishYear"
             type="number"
-            value={publishYear}
-            required
-            onChange={(e) => {
-              setPublishYear(Number(e.target.value));
-            }}
+            value={formik.values.publishYear}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             placeholder="publish year"
             crossOrigin={undefined}
           />
-
+          <div className="text-sm text-slate-700">
+            {formik.errors.publishYear &&
+              formik.touched.publishYear &&
+              formik.errors.publishYear}
+          </div>
           <div className="mt-60 flex ">
             <Button
               size="sm"
               variant="filled"
               placeholder={"undefined"}
-              onClick={handleSaveBook}
+              type="submit"
             >
               Save
             </Button>
           </div>
-        </div>
+        </form>
       </section>
     </div>
   );
--- frontend/src/pages/EditBook.tsx
+++ frontend/src/pages/EditBook.tsx
@@ -1,16 +1,28 @@
-import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { BACKEND_URL } from "./Home";
 import axios from "axios";
 import { Button, Input } from "@material-tailwind/react";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { useEffect, useState } from "react";
 
 const EditBook: React.FC = () => {
   const { id } = useParams<string>();
   const PATH: string = BACKEND_URL + "/books/" + id;
+
   const [title, setTitle] = useState<string>("");
   const [author, setAuthor] = useState<string>("");
   const [publishYear, setPublishYear] = useState<number>(0);
 
+  const navigate = useNavigate();
+  const bookSchema = Yup.object().shape({
+    title: Yup.string().required("Title is required"),
+    author: Yup.string().required("Author is required"),
+    publishYear: Yup.number()
+      .min(1000, `Publish year can't be less than 1000`)
+      .max(new Date().getFullYear(), `Publish year can't be in the future`)
+      .required("Publish year is required"),
+  });
   useEffect(() => {
     axios
       .get(PATH)
@@ -30,24 +42,26 @@ const EditBook: React.FC = () => {
         console.log(error);
       });
   }, [id]);
-  const navigate = useNavigate();
-
-  const handleSaveBook = () => {
-    const data = {
-      title,
-      author,
-      publishYear,
-    };
-    axios
-      .put(PATH, data)
-      .then(() => {
-        navigate("/");
-      })
-      .catch((error) => {
-        alert("An error occurred");
-        console.log(error);
-      });
-  };
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      title: title,
+      author: author,
+      publishYear: publishYear,
+    },
+    onSubmit: (values) => {
+      axios
+        .put(PATH, values)
+        .then(() => {
+          navigate("/");
+        })
+        .catch((error) => {
+          alert("An error occurred");
+          console.log(error);
+        });
+    },
+    validationSchema: bookSchema,
+  });
   return (
     <div className="mx-auto w-full max-w-7xl">
       <section
@@ -55,57 +69,69 @@ const EditBook: React.FC = () => {
         className="bg-white space-y-4 p-4 sm:px-8 sm:py-6 lg:p-4 xl:px-8 xl:py-6"
       >
         <div className="flex items-center justify-between">
-          <h2 className="font-semibold text-slate-900">Edit Book</h2>
+          <h2 className="font-semibold text-slate-900">Book Details</h2>
         </div>
       </section>
       <section className="p-4 sm:px-8 sm:pt-6 sm:pb-8 lg:p-4 xl:px-8 xl:pt-6 xl:pb-8 leading-6">
-        <div className="flex  md:w-1/2 w-full flex-col gap-6">
+        <form
+          onSubmit={formik.handleSubmit}
+          className="flex  md:w-1/2 w-full flex-col gap-6"
+        >
           <Input
             variant="outlined"
             label="title"
             placeholder="title"
-            value={title}
-            required
-            onChange={(e) => {
-              setTitle(e.target.value);
-            }}
+            name="title"
+            value={formik.values.title}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             crossOrigin={undefined}
           />
+          <div className="text-sm text-slate-700">
+            {formik.errors.title && formik.touched.title && formik.errors.title}
+          </div>
           <Input
             variant="outlined"
             label="author"
-            value={author}
+            value={formik.values.author}
             placeholder="author"
-            required
-            onChange={(e) => {
-              setAuthor(e.target.value);
-            }}
+            name="author"
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             crossOrigin={undefined}
           />
+          <div className="text-sm text-slate-700">
+            {formik.errors.author &&
+              formik.touched.author &&
+              formik.errors.author}
+          </div>
           <Input
             variant="outlined"
             label="publish year"
+            name="publishYear"
             type="number"
-            required
-            value={publishYear}
-            onChange={(e) => {
-              setPublishYear(Number(e.target.value));
-            }}
+            value={formik.values.publishYear}
+            onChange={formik.handleChange}
+            onBlur={formik.handleBlur}
             placeholder="publish year"
             crossOrigin={undefined}
           />
-
+          <div className="text-sm text-slate-700">
+            {formik.errors.publishYear &&
+              formik.touched.publishYear &&
+              formik.errors.publishYear}
+          </div>
           <div className="mt-60 flex ">
             <Button
               size="sm"
               variant="filled"
               placeholder={"undefined"}
-              onClick={handleSaveBook}
+              type="submit"
             >
               Save
             </Button>
           </div>
-        </div>
+        </form>
       </section>
     </div>
   );
